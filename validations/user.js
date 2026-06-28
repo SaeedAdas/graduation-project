90badaf77b407e3d8a59bd47313b859a90601bd7
SaeedAdas <adasssaeed@gmail.com>
2026-06-28 10:33:01 +0300

    Syntax error in update validation schema

--- validations/user.js
+++ validations/user.js
@@ -15,7 +15,7 @@ const update = z.object({
 	full_name: z.string(),
 	phone: z.string().regex(/^(?:\+97[02]5[69]\d{7}|05[69]\d{7})$/, {
   		message: "Invalid phone number, must start with 056 or 059 or +97259 or +97256 followed by 7 numbers"
-	})
+	}),
 	city: z.string(),
 	bio: z.string().max(1500, "Bio must be 1500 characters"),
 	birthdate: z.iso.date({
