1819f64a7e961edf05603f1e2619eab6a6bf74f1
SaeedAdas <adasssaeed@gmail.com>
2026-07-08 14:40:40 +0300

    fix: Prevent app crash because handlePrismaError assumed that each function returned the response

--- helper/messages.js
+++ helper/messages.js
@@ -1,19 +1,19 @@
 // Success codes with 200+
 
 exports.success = (res, message = "Operation successful") => {
-	res.status(200).json({
+	return res.status(200).json({
 		message: message 
 	});
 };
 
 exports.createdSuccessfully = (res, message = "Created successfully") => {
-	res.status(201).json({
+	return res.status(201).json({
 		message: message 
 	});
 };
 
 exports.deletedSuccessfully = (res) => {
-	res.status(204).send();
+	return res.status(204).send();
 };
 
 
@@ -21,48 +21,48 @@ exports.deletedSuccessfully = (res) => {
 
 exports.badRequest = (res, payload = "Bad request") => {
 	if (typeof payload === "object") {
-		res.status(400).json(payload);
+		return res.status(400).json(payload);
 	}
 	else { 
-		res.status(400).json({
+		return res.status(400).json({
 			issues: [payload]
 		});
 	}
 };
 
 exports.Unauthenticated = (res, message = "Unauthenticated") => {
-	res.status(401).json({
+	return res.status(401).json({
 		message
 	});
 };
 
 exports.Unauthorized = (res, message = "Forbidden") => {
-	res.status(403).json({
+	return res.status(403).json({
 		message
 	});
 };
 
 exports.notFound = (res, message = "Not found") => {
-	res.status(404).json({
+	return res.status(404).json({
 		message: message 
 	});
 };
 
 exports.alreadyExists = (res, message = "Already exists") => {
-	res.status(409).json({
+	return res.status(409).json({
 		message: message 
 	});
 };
 
 // Server errors with 500+
 exports.serverError = (res, message = "Server Error") => {
-	res.status(500).json({
+	return res.status(500).json({
 		message: message
 	});
 };
 
 exports.notImplemented = (res) => {
-	res.status(501).json({
+	return res.status(501).json({
 		message: "Not Implemented"
 	});
 };
