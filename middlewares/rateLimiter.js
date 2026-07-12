df610c51ade6c717ffacf58ddae727212207e0c3
SaeedAdas <adasssaeed@gmail.com>
2026-07-12 14:04:27 +0300

    feat: Implementing fixed-window rate-limiting algorithm

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,22 +1,64 @@
 const redis = require("../config/redis");
+const messages = require("../helper/messages");
 
 async function rateLimit(limit, window, identifier, scope) {
-	return (req, res, next) => {
-
+	try {
+		// Implemented Fixed-window algorithm
+		const now = Date.now()
 		const windowMs = window * 1000;
-		const windowId = Math.floor(Date.now() / windowMs);
+		const windowId = Math.floor(now / windowMs);
+		const windowEnd = (windowId + 1) * windowMs;
 
 		const key = `rl:${scope}:${identifier}:${windowId}`
 
-		const transaction = redis.multi();
+		const ttlSeconds = Math.max(
+        		1,
+        		Math.ceil((windowEnd - now) / 1000)
+    		);
 
-		transaction.incr(key);
-		transaction.expire(key, ttl);
+		// Atomic lua script to prevent calling expire after each increment 
+		const RATE_LIMIT_SCRIPT = `
+    			local count = redis.call("INCR", KEYS[1])
 
-		const results = await transaction.exec();
+    			if count == 1 then
+        			redis.call("EXPIRE", KEYS[1], tonumber(ARGV[1]))
+    			end
 
-		const record = await redis.get(identifier); 	
+    			return count
+		`;
+
+		const count = Number(
+			await redis.eval(
+				RATE_LIMIT_SCRIPT, 
+				[key], 
+				[ttlSeconds]
+			)
+		);
+
+		if (count > limit) return false;
+
+		return true;
+
+	} catch(error) {
+		console.error("Error in rate limiting retrieval: ", error);
+
+		return messages.serverError(res);
 	}
+
 }
 
-module.exports = { rateLimit };
+const loginRateLimiter = async (req, res, next) => {
+	const identifier = "ip:" + req.ip;
+	const message = "Too many login requests";
+
+	const isAllowed = await rateLimit(10, 3 * 60, identifier, "login");
+
+	if (!isAllowed) return messages.rateLimit(res, message);
+		
+	return next();
+};
+
+module.exports = { 
+	loginRateLimiter 
+};
+
